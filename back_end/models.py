553f02066d47d83eacec3535fe0d0207a7a42aae
Teodor Ganga <56924537+MartianCoders@users.noreply.github.com>
2021-10-16 13:55:55 +0300

    Add an example; Add models schema

--- back_end/models.py
+++ back_end/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+    # Curs
+      '''
+        Avem un curs care detine un map cu {titlu: list(tutoriale)}
+        Vezi LinkedIn Tutorials ca exemplu
+
+        Punctajul final (la final aratam cat are din toate)
+      '''
+
+        # -> Tutorial
+        '''
+        Tutorialul este video ul in sine cu ce mai are el
+        ~ title
+        ~ video
+        ~ duration
+        ~ Recenzie (obiect)
+        ~ descriere
+        '''
+
+    # Recenzie
+    '''
+    Aici votam tutorialul (stelute, numar de voturi, medie stelute)
+    '''
+
+    # Quiz
+    '''
+    Un fel de map cu intrebari si raspunsuri + un punctaj ce se adauga la cel final
+    '''
+
+    # Diploma
+    '''
+    O trimitem pe Mail generata cu numele lui si cursul absolvit
+    '''
+
+    # Grup
+    '''
+    Participanti (users) cu un fel de chat (POATE) unde trimite invitatii pe Mail
+    '''
+
+    # Mailing
+    '''
+    Aici handluim miling system ul. Constituim mail urile si etc
+    '''
+
--- django_proiect_colectiv/urls.py
+++ django_proiect_colectiv/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from back_end.views import index
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
 ]
