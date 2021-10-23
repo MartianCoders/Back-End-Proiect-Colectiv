3bb9ce53a3524f823603a10f5c8ef89091b4759f
paulhoban67 <paulhoban67@yahoo.com>
2021-10-23 13:12:44 +0300

    paulmGATA

--- back_end/models.py
+++ back_end/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+<<<<<<< HEAD
 # Curs
 '''
   Avem un curs care detine un map cu {titlu: list(tutoriale)}
@@ -63,3 +64,48 @@ class Rating(models.Model):
             self.__nrOfVotes,
             self.__starsAverage)
 
+=======
+    # Curs
+      '''
+        Avem un curs care detine un map cu {titlu: list(tutoriale)}
+        Vezi LinkedIn Tutorials ca exemplu
+
+        Punctajul final (la final aratam cat are din toate)
+      '''
+
+        # -> Tutorial
+        '''
+        Tutorialul este video ul in sine cu ce mai are el
+        ~ title
+        ~ video
+        ~ duration
+        ~ Recenzie (obiect)
+        ~ descriere
+        '''
+
+    # Recenzie
+    '''
+    Aici votam tutorialul (stelute, numar de voturi, medie stelute)
+    '''
+
+    # Quiz
+    '''
+    Un fel de map cu intrebari si raspunsuri + un punctaj ce se adauga la cel final
+    '''
+
+    # Diploma
+    '''
+    O trimitem pe Mail generata cu numele lui si cursul absolvit
+    '''
+
+    # Grup
+    '''
+    Participanti (users) cu un fel de chat (POATE) unde trimite invitatii pe Mail
+    '''
+
+    # Mailing
+    '''
+    Aici handluim miling system ul. Constituim mail urile si etc
+    '''
+    #paul
+>>>>>>> 953c996 (paul-GATA)
