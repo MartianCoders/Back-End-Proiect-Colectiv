ba06a0308102a91db1f57fc14163e0009702c30e
Matei Leonard <leonard.matei2011@yahoo.com>
2021-10-25 17:59:13 +0300

    Leo-added view more button on comments

--- frontend-client/src/components/Comment.js
+++ frontend-client/src/components/Comment.js
@@ -8,6 +8,7 @@ const Comment = ({ comments, removeComment, updateComment }) => {
     id: null,
     value: ''
   });
+  const [noOfCommentsToShow, setNoOfCommentsToShow]= useState(3)
 
   const submitUpdate = value => {
     updateComment(edit.id, value);
@@ -20,29 +21,41 @@ const Comment = ({ comments, removeComment, updateComment }) => {
   if (edit.id) {
     return <CommentForm edit={edit} onSubmit={submitUpdate} />;
   }
-
-  return comments.map((comment, index) => (
-    <>
-        <div class="card" id="comment-card">
-          <div class="card-body" id="comment-card-body">
-  
-          <span id="text"> 
-            <div key={comment.id} >
-              {comment.text}
-            </div> 
-          </span>
-
-          <div className="icons" id="icons" >
-            <DropdownButton id="dropdown-btn" title={<i class="bi bi-three-dots-vertical"></i>}>
-              <button id="btn-icon" onClick={() => setEdit({ id: comment.id, value: comment.text })}><Dropdown.Item href="#/action-1" ><i class="bi bi-pencil-square"></i> Edit</Dropdown.Item></button>
-              <button id="btn-icon" onClick={() => removeComment(comment.id)}><Dropdown.Item href="#/action-2"><i class="bi bi-trash-fill"></i> Delete</Dropdown.Item></button>
-            </DropdownButton>
+  //if number of comments is bigger than 3 then the view more button
+  // will appear, if you click view more it will show you more comments(10 or less)
+  return <>
+    {comments.map((comment, index) => {
+      if(index+1>noOfCommentsToShow) return false
+      return <>
+          <div class="card" id="comment-card">
+            <div class="card-body" id="comment-card-body">
+              <span id="text"> 
+                <div key={comment.id} >
+                  {comment.text}
+                </div> 
+              </span>
+              <div className="icons" id="icons" >
+                <DropdownButton id="dropdown-btn" title={<i class="bi bi-three-dots-vertical"></i>}>
+                  <button id="btn-icon" onClick={() => setEdit({ id: comment.id, value: comment.text })}><Dropdown.Item ><i class="bi bi-pencil-square"></i> Edit</Dropdown.Item></button>
+                  <button id="btn-icon" onClick={() => removeComment(comment.id)}><Dropdown.Item ><i class="bi bi-trash-fill"></i> Delete</Dropdown.Item></button>
+                </DropdownButton>
+              </div>
+            </div>
           </div>
-          </div>
-      </div>
-       
-    </>
-  ));
+        
+        </>
+      }
+    )
+    }
+    {noOfCommentsToShow<comments.length &&(
+      <button
+        onClick={()=>{setNoOfCommentsToShow(noOfCommentsToShow+10)}}
+      >
+        View More
+      </button>
+      )
+    }
+</>
 };
 
 
