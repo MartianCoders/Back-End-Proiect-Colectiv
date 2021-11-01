e555ebf5a8d865931bba88ac617857b51ad99f98
DanTheStud <xilisiedanx@gmail.com>
2021-11-01 23:04:31 +0200

    Modified compoents to use Class

--- frontend-client/src/components/courseOverview.tsx
+++ frontend-client/src/components/courseOverview.tsx
@@ -4,37 +4,39 @@ import SearchIcon from '@mui/icons-material/Search';
 
 
 
-const CourseOverview = () => {
+class CourseOverview extends React.Component {
+    render() {
 
-    const [bar, switchBar] = useState(1);
+        const [bar, switchBar] = useState(1);
 
-    return (
-        <div>
-            <div style={{ width: "1020px" }}>
-                <Navbar bg="light" variant="light">
-                    <Container>
-                        <SearchIcon />
-                        <Nav className="me-auto">
-                            <Nav.Link onClick={() => { switchBar(1) }}>Overview</Nav.Link>
-                            <Nav.Link onClick={() => { switchBar(2) }}>Q&A</Nav.Link>
-                            <Nav.Link onClick={() => { switchBar(3) }}>Notes</Nav.Link>
-                            <Nav.Link onClick={() => { switchBar(4) }}>Announcements</Nav.Link>
-                        </Nav>
-                    </Container>
-                    
-                </Navbar>
-                <div>
-                        {bar == 1 ? <div style={{display : "flex", justifyContent : "flex-start", flexDirection : "row"}}>
+        return (
+            <div>
+                <div style={{ width: "1020px" }}>
+                    <Navbar bg="light" variant="light">
+                        <Container>
+                            <SearchIcon />
+                            <Nav className="me-auto">
+                                <Nav.Link onClick={() => { switchBar(1) }}>Overview</Nav.Link>
+                                <Nav.Link onClick={() => { switchBar(2) }}>Q&A</Nav.Link>
+                                <Nav.Link onClick={() => { switchBar(3) }}>Notes</Nav.Link>
+                                <Nav.Link onClick={() => { switchBar(4) }}>Announcements</Nav.Link>
+                            </Nav>
+                        </Container>
+
+                    </Navbar>
+                    <div>
+                        {bar == 1 ? <div style={{ display: "flex", justifyContent: "flex-start", flexDirection: "row" }}>
                             This is an overview.
-                        </div> : bar == 2 ? <div style={{display : "flex", justifyContent : "flex-start", flexDirection : "row"}}>
+                        </div> : bar == 2 ? <div style={{ display: "flex", justifyContent: "flex-start", flexDirection: "row" }}>
                             Add questions.
-                        </div> : bar == 3 ? <div style={{display : "flex", justifyContent : "flex-start", flexDirection : "row"}}>
+                        </div> : bar == 3 ? <div style={{ display: "flex", justifyContent: "flex-start", flexDirection: "row" }}>
                             Notes.
-                        </div> : bar == 4 ? <div style={{display : "flex", justifyContent : "flex-start", flexDirection : "row"}}>
+                        </div> : bar == 4 ? <div style={{ display: "flex", justifyContent: "flex-start", flexDirection: "row" }}>
                             Announcements.
                         </div> : null}
                     </div>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }; export default CourseOverview;
--- frontend-client/src/components/watchCourse.tsx
+++ frontend-client/src/components/watchCourse.tsx
@@ -2,23 +2,25 @@ import React, { Component, useState } from "react";
 import ReactPlayer from 'react-player/lazy'
 import CourseOverview from "./courseOverview";
 
-const WatchCourse = () => {
+class WatchCourse extends React.Component {
+    render() {
 
-    return (
-        
-        <div>
+        return (
+
             <div>
-                <ReactPlayer 
-                width = "1020px" 
-                height = "450px" 
-                url='https://www.youtube.com/watch?v=Y-OLcnr8eNo&ab_channel=VivekJoy'
-                controls
-                />
+                <div>
+                    <ReactPlayer
+                        width="1020px"
+                        height="450px"
+                        url='https://www.youtube.com/watch?v=Y-OLcnr8eNo&ab_channel=VivekJoy'
+                        controls
+                    />
+                </div>
+                <div style={{ display: "flex", justifyContent: "flex-start", flexDirection: "row" }}>
+                    <CourseOverview />
+                </div>
             </div>
-            <div style={{display : "flex", justifyContent : "flex-start", flexDirection : "row"}}>
-                <CourseOverview/>
-            </div>
-        </div>
-    );
+        );
+    }
 }; export default WatchCourse;
 
