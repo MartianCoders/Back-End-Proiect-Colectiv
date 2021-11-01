2373b383780a0f4acb3f11cedad4ffefbfeb9718
Iulia030500 <iulia.mahmote@gmail.com>
2021-11-01 23:51:21 +0200

    Task1 done

--- back_end/models.py
+++ back_end/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
     # Curs
-      '''
+'''
         Avem un curs care detine un map cu {titlu: list(tutoriale)}
         Vezi LinkedIn Tutorials ca exemplu
 
@@ -11,7 +11,7 @@ from django.db import models
       '''
 
         # -> Tutorial
-        '''
+'''
         Tutorialul este video ul in sine cu ce mai are el
         ~ title
         ~ video
@@ -21,29 +21,37 @@ from django.db import models
         '''
 
     # Recenzie
-    '''
+'''
     Aici votam tutorialul (stelute, numar de voturi, medie stelute)
     '''
 
     # Quiz
-    '''
+'''
     Un fel de map cu intrebari si raspunsuri + un punctaj ce se adauga la cel final
     '''
 
     # Diploma
-    '''
+'''
     O trimitem pe Mail generata cu numele lui si cursul absolvit
     '''
 
     # Grup
-    '''
+'''
     Participanti (users) cu un fel de chat (POATE) unde trimite invitatii pe Mail
     '''
 
     # Mailing
-    '''
+'''
     Aici handluim miling system ul. Constituim mail urile si etc
     '''
 
-    "Iulia"
+class Comment(models.Model):
+    userName = models.CharField(max_lenght = 50)
+    content = models.CharField(max_lenght = 1000)
 
+    def _init_(self, userName, content):
+        self.userName = userName
+        self.content = content
+
+    def _str_(self):
+        return '{}: {}'.format(self.userName, self.content)
