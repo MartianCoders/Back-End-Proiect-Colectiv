6b6a1300d5b4b0def86e49c07a72a1a04f9e1223
isaisebastian <sebastianisai14@outlook.com>
2021-11-02 22:23:26 +0200

    switch to class component

--- frontend-client/src/components/CourseCard.tsx
+++ frontend-client/src/components/CourseCard.tsx
@@ -1,29 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './CourseCard.css';
 import {Button} from 'react-bootstrap';
+interface ICourseProps{
+    courseTitle: string;
+    imgSrc: string;
+    videos: number;
+    rating: number;
+    noOfStudents: string;
+    price: string;
+}
+interface ICourseState{
 
-function CourseCard(props: any) {
-    return (
-        <div className="courseCard">
-            <img className="courseImg" src={props.imgSrc} alt="courseImg"></img>
-            <h3>{props.courseTitle}</h3>
-
-            <hr/>
-            <p><i className="fa fa-play-circle"></i>{props.videos} videos</p>
-            <div className="ratingDiv">
-                <div className="rating">
-                    <span className="stars">
-                        &#11088;
-                    </span>
-                    {props.rating}
-                    <span className="noOfStudents">&nbsp;{props.noOfStudents}</span>
-                </div>
-                <h3 className="price">{props.price}</h3>
-            </div>
-
-            <Button className="joinBtn">Start course</Button>
-        </div>
-    )
 }
 
-export default CourseCard
+export default class CourseCard extends Component<ICourseProps, ICourseState> {
+    render() : JSX.Element {
+        const {courseTitle, imgSrc, videos, rating, noOfStudents, price}: ICourseProps = this.props;
+        return (
+            <div className="courseCard">
+                <img className="courseImg" src={imgSrc} alt="courseImg"></img>
+                <h3>{courseTitle}</h3>
+
+                <hr/>
+                <p><i className="fa fa-play-circle"></i>{videos} videos</p>
+                <div className="ratingDiv">
+                    <div className="rating">
+                        <span className="stars">
+                            &#11088;
+                        </span>
+                        {rating}
+                        <span className="noOfStudents">&nbsp;{noOfStudents}</span>
+                    </div>
+                    <h3 className="price">{price}</h3>
+                </div>
+
+            <Button className="joinBtn">Start course</Button>
+            </div>
+        )
+    }
+}
