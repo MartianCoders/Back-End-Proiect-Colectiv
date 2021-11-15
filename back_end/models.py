45d37d07a3f12c94a01314dddf1bf2a6ab1862e1
Teodor <56924537+MartianCoders@users.noreply.github.com>
2021-11-15 17:20:56 +0200

    Fix models.py

--- back_end/models.py
+++ back_end/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
+
+# Create your models here.
+# modificare
+
 # Category Model
 class Category(models.Model):
-  title=models.CharField(max_length=30)
-  idList=models.ManyToOneRel(Course)
-def __init__(self,title,idList):
-  self.__title=title
-  self.__idList=idList
-def __str__(self):
-   return "{}:{}".format(self.__title,self.__idList)
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return "{}".format(self.title)
 
 
 class Course(models.Model):
@@ -26,15 +27,16 @@ class Tutorial(models.Model):
     video = models.FileField(upload_to='videos/')
     categoryID = models.IntegerField()
     description = models.CharField(max_length=256)
-    rating =models.ForeignKey(Rating, on_delete=models.CASCADE, related_name='ratings')
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='tutorials')
+    image = models.ImageField(upload_to='images/')
 
     def __str__(self):
-        return 'Tutorial: {} {} {} {}'.format(
+        return 'Tutorial: {} {} {} {} {}'.format(
             self.id,
             self.video,
             self.categoryID,
-            self.description)
+            self.description,
+            self.image)
 
 
 class Rating(models.Model):
