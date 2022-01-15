a8547f29ade58c31c919ce97c487c64f0addb97b
Teodor <56924537+MartianCoders@users.noreply.github.com>
2022-01-15 15:38:28 +0200

    Revert "Pcbe 18 check answers of quiz"

--- back_end/admin.py
+++ back_end/admin.py
@@ -1,13 +1,4 @@
 from django.contrib import admin
-from back_end.models import *
 
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Course)
-admin.site.register(Tutorial)
-admin.site.register(Rating)
-admin.site.register(Comment)
-admin.site.register(Review)
-admin.site.register(Quiz)
-admin.site.register(Question)
